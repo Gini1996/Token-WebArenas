0c38c913e50cd92203aa3e5bfcf0e629d97f8f3e
Leonardo Ferreira <leonardo.gini.96@hotmail.com>
2023-09-14 23:30:48 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/webarenas/security/token/gerartoken/TokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webarenas.security.token.gerartoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
